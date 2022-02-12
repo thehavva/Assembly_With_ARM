6d510e3d8616a70981ac6378dbaa2c5b7c64d25d
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2022-02-12 17:53:42 +0300

    Add files via upload

---
+++ Logic-Shift/code4.s
@@ -0,0 +1,8 @@
+       ;logic  operation insttructions
+
+       MOV     R0, #0xFF
+       MOV     R1, #0xAA
+
+       AND     R2, R0, R1
+       ORR     R3, R0, R1
+       EOR     R4, R0, R1
---
+++ Logic-Shift/code5.s
@@ -0,0 +1,6 @@
+       MOV     R0, #0x01
+       MOV     R2, #0x0A
+
+       ;shift  operations
+       LSL     R1, R0, #2
+       LSR     R3, R2, #3
